667ce00edb01aef813669d672d8a7ba81e799711
yaohsienhuang <hsien.huang@outlook.com>
2022-11-13 13:37:07 +0800

    first commit

---
+++ twoSum.py
@@ -0,0 +1,20 @@
+from typing import List, Set, Dict, Tuple
+
+class Solution():
+    '''
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    '''
+    def twoSum(self, nums:List[int], target:int) -> List[int]:
+        dict={}
+        for idx,value in enumerate(nums):
+            if target-value in dict:
+                return [dict[target-value],idx]
+            dict[value]=idx
+
+answer=Solution().twoSum(
+    nums=[2,7,11,15],
+    target= 9
+)
+print(answer)
