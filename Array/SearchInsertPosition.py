a26f155fe2a8c580ff0a50dca2a7b979957c5162
yaohsienhuang <hsien.huang@outlook.com>
2022-11-19 22:03:06 +0800

    feat:improve searhInsertPoistion

--- Array/SearchInsertPosition.py
+++ Array/SearchInsertPosition.py
@@ -18,19 +18,27 @@ class Solution:
 
     def searchInsert_binarySearch(self, nums: List[int], target: int) -> int:
         '''
-        Runtime: 113 ms, faster than 27.22% of Python3 online submissions for Search Insert Position.
+        Runtime: 62 ms, faster than 81.45% of Python3 online submissions for Search Insert Position.
         '''
         low, high = 0, len(nums)
         while low < high:
             mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
             if target > nums[mid]:
                 low = mid + 1
             else:
                 high = mid
         return low
 
+    def searchInsert_shorten(self, nums: List[int], target: int) -> int:
+        '''
+        Runtime: 118 ms, faster than 16.46% of Python3 online submissions for Search Insert Position.
+        '''
+        return len([x for x in nums if x<target])
+
 if __name__ == '__main__':
-    answer=Solution().searchInsert_binarySearch(
+    answer=Solution().searchInsert_shorten(
         #nums = [1,3,5,6], target = 7
         nums = [1,3,5,6], target = 5
     )
